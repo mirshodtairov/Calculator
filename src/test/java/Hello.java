7e0f8663dc87503bbd7fe73e23865d8cce610b61
mirshodtairov <50146656+mirshodtairov@users.noreply.github.com>
2019-08-07 13:59:31 -0400

    add 2 more methods
    added: Multiply and Divide

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -9,5 +9,8 @@ public class Hello {
     public static double multiply(double num1, double num2){
       return num1*num2;
   }
+     public static double divide(double num1, double num2){
+      return num1/num2;
+  }
  
 }
